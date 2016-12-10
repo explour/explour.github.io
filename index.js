1c03d7546496893be789720625e51c9588fd4d15
Wilson Lau <wilson@Wilsons-MacBook-Air.local>
2016-12-10 12:10:57 -0800

    Add archives

--- index.js
+++ index.js
@@ -6,4 +6,45 @@ $(document).ready(function() {
   $('#sneak-peak-button').click(function(){
     $('.pop-up-wrapper').css("display", "flex");
   });
+
+  $('html').keydown(function(e){
+    if (e.keyCode == 39) {
+      e.preventDefault();
+      moveForward();
+    }
+    if (e.keyCode == 37) {
+      e.preventDefault();
+      moveBack();
+    }
+});
+
+  $('#front-arrow').click(function(){
+    moveForward();
+  });
+
+  $('#back-arrow').click(function(){
+    moveBack();
+  });
+
+  function moveForward() {
+    $(".focus-container").next().addClass("focus-container");
+    $(".focus-container").next().removeClass("hidden-container");
+    $(".focus-container").prev().addClass("hidden-container");
+    $(".focus-container").prev().removeClass("focus-container");
+    $(".focus-container").next().removeClass("focus-container");
+    $(".focus-container").next().addClass("hidden-container");
+  }
+
+  function moveBack() {
+    $(".focus-container").prev().addClass("focus-container");
+    $(".focus-container").prev().removeClass("hidden-container");
+    $(".focus-container").next().addClass("hidden-container");
+    $(".focus-container").next().removeClass("focus-container");
+    $(".focus-container").prev().removeClass("focus-container");
+    $(".focus-container").prev().addClass("hidden-container");
+  }
+
+
+
+
 })
